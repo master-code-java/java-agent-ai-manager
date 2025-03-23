202e047b19b080377d4c8a7bfc1ff6d54c4216ab
master-code-java <t.r.assis@gmail.com>
2025-03-23 22:37:44 +0000

    Project Service method creation

--- src/main/java/com/oiseau/ai_agent_tester/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/oiseau/ai_agent_tester/service/impl/ProjectServiceImpl.java
@@ -1,7 +1,10 @@
 package com.oiseau.ai_agent_tester.service.impl;
 
+import com.oiseau.ai_agent_tester.model.Agent;
 import com.oiseau.ai_agent_tester.model.Project;
+import com.oiseau.ai_agent_tester.repository.AgentRepository;
 import com.oiseau.ai_agent_tester.repository.ProjectRepository;
+import com.oiseau.ai_agent_tester.request.ProjectRequest;
 import com.oiseau.ai_agent_tester.service.ProjectService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +20,9 @@ public class ProjectServiceImpl implements ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private AgentRepository agentRepository;
+
     @Override
     public List<Project> getAllProjects() {
         return projectRepository.findAll();
@@ -29,16 +35,25 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public Project createProject(Project project) {
-        project.setUuid(UUID.randomUUID());
+        project.setUuid(UUID.randomUUID().toString());
         return projectRepository.save(project);
     }
 
     @Override
     public Optional<Project> updateProject(int id, Project projectDetails) {
-        return projectRepository.findById(id).map(project -> {
-            project.setAgents(projectDetails.getAgents());
-            return projectRepository.save(project);
+        return projectRepository.findById(id).map(existingProject -> {
+            if (projectDetails.getUuid() != null) {
+                existingProject.setUuid(projectDetails.getUuid());
+
+            }
+            if (projectDetails.getAgents() != null && !projectDetails.getAgents().isEmpty()) {
+                existingProject.setAgents(projectDetails.getAgents());
+
+            }
+            return projectRepository.save(existingProject);
+
         });
+
     }
 
     @Override
@@ -49,5 +64,15 @@ public class ProjectServiceImpl implements ProjectService {
         } else {
             return false;
         }
+    }
+
+    @Override
+    public Project createProject(ProjectRequest projectRequest) {
+        Project newProject = new Project();
+        newProject.setUuid(UUID.randomUUID().toString());
+        List<Agent> agents = agentRepository.findAllByUuidIn(projectRequest.getProjectsUUID());
+        newProject.setAgents(agents);
+
+        return projectRepository.save(newProject);
     }
 }
