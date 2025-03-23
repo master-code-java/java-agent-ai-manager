2f851eaa342d38088a4cc48e36120036bd2cef18
master-code-java <t.r.assis@gmail.com>
2025-03-23 22:36:55 +0000

    Change method findByUuid to receive string

--- src/main/java/com/oiseau/ai_agent_tester/repository/ProjectRepository.java
+++ src/main/java/com/oiseau/ai_agent_tester/repository/ProjectRepository.java
@@ -4,9 +4,8 @@ import com.oiseau.ai_agent_tester.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
-    Project findByUuid(UUID uuid);
+    Project findByUuid(String uuid);
 }
