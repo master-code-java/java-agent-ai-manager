cdfe0630374eb64380d679c64c7c34c054a1da76
master-code-java <t.r.assis@gmail.com>
2025-03-24 01:02:29 +0000

    refactor ProjectRepository to return Optional<Project> for findByUuid method

--- src/main/java/com/oiseau/ai_agent_tester/repository/ProjectRepository.java
+++ src/main/java/com/oiseau/ai_agent_tester/repository/ProjectRepository.java
@@ -1,11 +1,15 @@
 package com.oiseau.ai_agent_tester.repository;
 
 import com.oiseau.ai_agent_tester.model.Project;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
-    Project findByUuid(String uuid);
+    Optional<Project> findByUuid(String uuid);
+
 }
