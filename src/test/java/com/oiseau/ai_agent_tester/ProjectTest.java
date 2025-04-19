792b427bfe0bb4719987637108ad260cd631c993
master-code-java <t.r.assis@gmail.com>
2025-04-19 22:12:19 +0100

    Interfaces for Agent and Execution

--- src/test/java/com/oiseau/ai_agent_tester/ProjectTest.java
+++ src/test/java/com/oiseau/ai_agent_tester/ProjectTest.java
@@ -7,46 +7,58 @@ import java.util.List;
 
 import org.junit.jupiter.api.*;;
 
-class Agent{
+class NaturalLanguageAgent implements NaturalLanguage{
 
+	private final String NATURAL_LANGUAGE = "NaturalLanguage";
 	private String context;
 
-	Agent(String context){
+	@Override
+	public void context(String context) {
 		this.context = context;
+	
 	}
-
+	
+	@Override
 	public String getContext() {
-		return context;
+		return this.context;
+
+	}		
+
+	@Override
+	public String getType() {
+		return this.NATURAL_LANGUAGE;
+	
 	}
+}
+
+interface Agent{
+	void context(String context);
+	String getContext();
+	String getType();
 
 }
 
-class Project{
+interface NaturalLanguage extends Agent{
 
-	private String initialInput;
-	private List<AgentSequence> sequence = new ArrayList<>();
+}
 
+interface Execution{}
+
+interface SequencialExecution extends Execution {
+	List<ResponseExecution> run(String input, List<AgentSequence> sequence);
+
+}
+
+class Project implements SequencialExecution{
+
+	/* for mock purpose only */
 	OpenAiService openAiService = new OpenAiService();
 
-
-	Project(String input, List<AgentSequence> sequence){
-		this.initialInput = input;
-		this.sequence = sequence;
-	}
-
-	public String getInitialInput() {
-		return initialInput;
-	}
-
-	public List<AgentSequence> getSequence() {
-		return sequence;
-	}
-
-	public List<ResponseExecution> run(){
+	public List<ResponseExecution> run(String input, List<AgentSequence> sequence){
 		List<ResponseExecution> responseExecutions = new ArrayList<>();
-		String[] currentInput = { this.initialInput };
+		String[] currentInput = { input };
 
-		this.sequence.stream()
+		sequence.stream()
 			.sorted((o1, o2) -> Integer.compare(o1.getPosition(), o2.getPosition()))
 			.forEachOrdered(agentSequence -> {			
 				String[] agentResponse = {openAiService.getResponse(agentSequence.getAgent().getContext(), currentInput[0])};
@@ -144,18 +156,22 @@ class ProjectTest {
 
 	@Test
 	void givenOrderedSequence_whenRunProject_thenRunOrderedSequence(){
-		Agent a1 = new Agent("Voce traduz para Arabe");
-		Agent a2 = new Agent("Voce traduz para Ingles");
-		Agent a3 = new Agent("Voce traduz para Frances");
+		NaturalLanguage nlp1 = new NaturalLanguageAgent();
+		nlp1.context("Voce traduz para Arabe");
 
-		List<AgentSequence> sequence = new ArrayList<>();
-		sequence.add(new AgentSequence(1, a1));
-		sequence.add(new AgentSequence(2,a2));
-		sequence.add(new AgentSequence(3, a3));
+		NaturalLanguage nlp2 = new NaturalLanguageAgent();
+		nlp2.context("Voce traduz para Ingles");
+
+		NaturalLanguage nlp3 = new NaturalLanguageAgent();
+		nlp3.context("Voce traduz para Frances");
 		
-		Project p1 = new Project("Ola", sequence);
-
-		List<ResponseExecution> result = p1.run();
+		List<AgentSequence> sequence = new ArrayList<>();
+		sequence.add(new AgentSequence(1, nlp1));
+		sequence.add(new AgentSequence(2,nlp2));
+		sequence.add(new AgentSequence(3, nlp3));
+		
+		Project p1 = new Project();
+		List<ResponseExecution> result = p1.run("Ola", sequence);
 
 		assertAll("", () -> {
 			assert result.get(0).getPosition() == 1;
@@ -181,18 +197,22 @@ class ProjectTest {
 
 	@Test
 	void givenUnorderedSequence_whenRunProject_thenRunOrderedSequence(){
-		Agent a1 = new Agent("Voce traduz para Arabe");
-		Agent a2 = new Agent("Voce traduz para Ingles");;
-		Agent a3 = new Agent("Voce traduz para Frances");
+		NaturalLanguage nlp1 = new NaturalLanguageAgent();
+		nlp1.context("Voce traduz para Arabe");
 
+		NaturalLanguage nlp2 = new NaturalLanguageAgent();
+		nlp2.context("Voce traduz para Ingles");
+
+		NaturalLanguage nlp3 = new NaturalLanguageAgent();
+		nlp3.context("Voce traduz para Frances");
+		
 		List<AgentSequence> sequence = new ArrayList<>();
-		sequence.add(new AgentSequence(3, a3));
-		sequence.add(new AgentSequence(2,a2));
-		sequence.add(new AgentSequence(1, a1));
+		sequence.add(new AgentSequence(3, nlp3));
+		sequence.add(new AgentSequence(1,nlp1));
+		sequence.add(new AgentSequence(2, nlp2));
 
-		Project p1 = new Project("Ola", sequence);
-
-		List<ResponseExecution> result = p1.run();
+		Project p1 = new Project();
+		List<ResponseExecution> result = p1.run("Ola", sequence);
 
 		assertAll("", () -> {
 			assert result.get(0).getPosition() == 1;
@@ -214,44 +234,5 @@ class ProjectTest {
 		});
 
 	}		
-
-	@Test
-	void givenUnorderedAgentsList_whenRunProject_thenRunOrderedSequence(){
-		Agent a1 = new Agent("Voce traduz para Arabe");
-		Agent a2 = new Agent("Voce traduz para Ingles");
-		Agent a3 = new Agent("Voce traduz para Frances");
-
-		List<AgentSequence> sequence = new ArrayList<>();
-		sequence.add(new AgentSequence(1, a1));
-		sequence.add(new AgentSequence(2,a2));
-		sequence.add(new AgentSequence(3, a3));
-
-		Project p1 = new Project("Ola", sequence);
-
-		List<ResponseExecution> result = p1.run();
-
-		assertAll("", () -> {
-			assert result.get(0).getPosition() == 1;
-			assert result.get(1).getPosition() == 2;
-			assert result.get(2).getPosition() == 3;
-
-			assert result.get(0).getContext().equals("Voce traduz para Arabe");
-			assert result.get(1).getContext().equals("Voce traduz para Ingles");
-			assert result.get(2).getContext().equals("Voce traduz para Frances");
-
-			assert result.get(0).getInput().equals("Ola");
-			assert result.get(1).getInput().equals("Ola em Arabe");
-			assert result.get(2).getInput().equals("Ola! em ingles");
-			
-			assert result.get(0).getResponse().equals("Ola em Arabe");	
-			assert result.get(1).getResponse().equals("Ola! em ingles");
-			assert result.get(2).getResponse().equals("Ola! em frances");
-
-		});
-		
-	}
-
-
-
 
 }
